28b54ad66298fd444f3ec99ea735a81a9bbe396c
Abbas-jbot <5dcwmq68gm@privaterelay.appleid.com>
2026-05-22 23:27:12 +0100

    Fix ball speed inversion logic in script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function moveBall() {
   ballY += ballSpeedY;
 
   if (ballY <= 0 || ballY >= canvas.height) {
-    ballSpeedY *= -10;
+    ballSpeedY *= -1;
   }
 
   // Player paddle collision
@@ -60,7 +60,7 @@ function moveBall() {
     ballY > playerY &&
     ballY < playerY + paddleHeight
   ) {
-    ballSpeedX *= -10;
+    ballSpeedX *= -1;
   }
 
   // AI paddle collision
@@ -69,7 +69,7 @@ function moveBall() {
     ballY > aiY &&
     ballY < aiY + paddleHeight
   ) {
-    ballSpeedX *= -10;
+    ballSpeedX *= -1;
   }
 
   // Reset ball
